c6305e042b5fb16a5a01827d9529a744784ff01c
Javier Arturo Carvajal Coronado <45467468+JaCarvajalCoronado@users.noreply.github.com>
2020-04-05 21:22:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/materialize.min.css">
-    <title>Login</title>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    < título > Iniciar sesión </ título >
 </head>
 <body>
 <div class="container">
